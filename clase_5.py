b5165695e258b4ecbc0a5dfae10ecfeb292a350f
gustavomansilla86 <gustavomansilla1986@hotmail.com>
2024-05-21 18:50:35 -0300

    iniciando

---
+++ clase_5.py
@@ -0,0 +1,4 @@
+marcas = ["Toyota", "Ford", "Honda", "Chevrolet", "Nissan", "Hyundai", "Subaru", "Volkswagen", "Mercedes-Benz", "BMW"]
+print(marcas)
+for indice,marcas in enumerate(marcas):
+    print(indice,marcas)
